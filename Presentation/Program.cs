8612a0e5d086bb5574e4e10a9b7b4348a9d86ebe
Rafeef87 <rafeefkhalifah1987@gmail.com>
2025-06-07 14:12:51 +0200

    Add swagger.

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddMemoryCache();
 builder.Services.AddSingleton(x => new EmailClient(builder.Configuration["ACS:ConnectionStrings"]));
@@ -13,6 +14,13 @@ builder.Services.AddTransient<IVerificationService, VerificationService>();
 
 var app = builder.Build();
 app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Verification Service API");
+    c.RoutePrefix = string.Empty; // Set the Swagger UI at the app's root
+});
+
 app.UseHttpsRedirection();
 app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
